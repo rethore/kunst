0f248d072eb31f92594374640603468f257fe21a
Pierre-Elouan <pe@retho.re>
2016-04-06 17:42:29 +0200

    adding support for download

--- kunst/client/main.js
+++ kunst/client/main.js
@@ -5,7 +5,153 @@ import { Hue } from '../imports/api/hue.js';
 import './main.html';
 
 
-const IP = 'ip322.alb283.cust.comxnet.dk:6785';
+var colormaps = [{cname:"gist_gray"},
+        {cname:"RdPu_r"},
+        {cname:"Set3_r"},
+        {cname:"gray"},
+        {cname:"Set2"},
+        {cname:"gist_ncar_r"},
+        {cname:"gist_ncar"},
+        {cname:"Accent"},
+        {cname:"YlGn"},
+        {cname:"RdYlBu"},
+        {cname:"BrBG_r"},
+        {cname:"Pastel2_r"},
+        {cname:"YlGn_r"},
+        {cname:"hot_r"},
+        {cname:"YlOrRd_r"},
+        {cname:"Spectral_r"},
+        {cname:"BrBG"},
+        {cname:"Purples"},
+        {cname:"terrain_r"},
+        {cname:"PuRd"},
+        {cname:"summer_r"},
+        {cname:"gist_heat"},
+        {cname:"flag_r"},
+        {cname:"gist_earth_r"},
+        {cname:"PuBuGn"},
+        {cname:"PuBu"},
+        {cname:"gist_gray_r"},
+        {cname:"hsv_r"},
+        {cname:"binary_r"},
+        {cname:"PiYG_r"},
+        {cname:"gist_yarg"},
+        {cname:"OrRd"},
+        {cname:"GnBu_r"},
+        {cname:"coolwarm_r"},
+        {cname:"RdYlGn"},
+        {cname:"spectral"},
+        {cname:"Wistia_r"},
+        {cname:"Dark2"},
+        {cname:"PuOr_r"},
+        {cname:"PiYG"},
+        {cname:"seismic"},
+        {cname:"RdPu"},
+        {cname:"Spectral"},
+        {cname:"Reds"},
+        {cname:"Oranges_r"},
+        {cname:"nipy_spectral"},
+        {cname:"Blues"},
+        {cname:"YlOrBr_r"},
+        {cname:"gist_stern_r"},
+        {cname:"CMRmap"},
+        {cname:"PRGn_r"},
+        {cname:"Greens_r"},
+        {cname:"BuGn_r"},
+        {cname:"RdBu"},
+        {cname:"nipy_spectral_r"},
+        {cname:"PuRd_r"},
+        {cname:"Accent_r"},
+        {cname:"jet_r"},
+        {cname:"BuPu"},
+        {cname:"Set1"},
+        {cname:"gist_rainbow"},
+        {cname:"gnuplot2_r"},
+        {cname:"Reds_r"},
+        {cname:"Greys_r"},
+        {cname:"prism"},
+        {cname:"rainbow"},
+        {cname:"Purples_r"},
+        {cname:"bwr_r"},
+        {cname:"Pastel1"},
+        {cname:"YlGnBu_r"},
+        {cname:"gist_yarg_r"},
+        {cname:"spectral_r"},
+        {cname:"hot"},
+        {cname:"RdGy_r"},
+        {cname:"gist_earth"},
+        {cname:"spring"},
+        {cname:"RdBu_r"},
+        {cname:"bone"},
+        {cname:"BuPu_r"},
+        {cname:"cubehelix"},
+        {cname:"autumn"},
+        {cname:"PuOr"},
+        {cname:"brg"},
+        {cname:"brg_r"},
+        {cname:"RdGy"},
+        {cname:"bwr"},
+        {cname:"Dark2_r"},
+        {cname:"Pastel2"},
+        {cname:"cool_r"},
+        {cname:"copper"},
+        {cname:"PuBuGn_r"},
+        {cname:"afmhot_r"},
+        {cname:"binary"},
+        {cname:"OrRd_r"},
+        {cname:"YlOrRd"},
+        {cname:"hsv"},
+        {cname:"afmhot"},
+        {cname:"gist_stern"},
+        {cname:"gnuplot"},
+        {cname:"Blues_r"},
+        {cname:"rainbow_r"},
+        {cname:"Pastel1_r"},
+        {cname:"gnuplot_r"},
+        {cname:"YlGnBu"},
+        {cname:"PRGn"},
+        {cname:"gray_r"},
+        {cname:"bone_r"},
+        {cname:"prism_r"},
+        {cname:"BuGn"},
+        {cname:"PuBu_r"},
+        {cname:"RdYlGn_r"},
+        {cname:"CMRmap_r"},
+        {cname:"YlOrBr"},
+        {cname:"pink_r"},
+        {cname:"Paired_r"},
+        {cname:"Greens"},
+        {cname:"pink"},
+        {cname:"spring_r"},
+        {cname:"seismic_r"},
+        {cname:"Set1_r"},
+        {cname:"Oranges"},
+        {cname:"RdYlBu_r"},
+        {cname:"cubehelix_r"},
+        {cname:"jet"},
+        {cname:"ocean_r"},
+        {cname:"terrain"},
+        {cname:"copper_r"},
+        {cname:"flag"},
+        {cname:"ocean"},
+        {cname:"GnBu"},
+        {cname:"winter"},
+        {cname:"winter_r"},
+        {cname:"autumn_r"},
+        {cname:"gist_heat_r"},
+        {cname:"cool"},
+        {cname:"gnuplot2"},
+        {cname:"Wistia"},
+        {cname:"Set2_r"},
+        {cname:"Set3"},
+        {cname:"Paired"},
+        {cname:"coolwarm"},
+        {cname:"summer"},
+        {cname:"gist_rainbow_r"},
+        {cname:"Greys"}];
+
+Session.setDefault("date", [4,4,2016]);
+
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
@@ -26,192 +172,53 @@ Template.hello.events({
 });
 
 Template.production.rendered=function() {
-	$('#my-datepicker').datepicker();
+	$('#my-datepicker').datepicker({format: "d/m/yyyy"});
 }
+
+Template.production.helpers({
+  disabled() {return (Hue.findOne({name:'active'}))? (Hue.findOne({name:'active'}).val)? "disabled" : "" : ""},
+  date() {
+    let [day, month, year] = Session.get('date')
+    return `${day}/${month}/${year}`
+  },
+  url(){
+    let date = $('#my-datepicker').val();
+    let [day, month, year] = Session.get('date');
+    let data = Hue.findOne({name:'data', day, month, year});
+    console.log('data',date)
+    return (data)? data.url : 'https://plot.ly/~piredtu/36';
+    //return 'https://plot.ly/~piredtu/36';
+  }
+});
+
+Template.production.events({
+  'click #getdate'(event, instance){
+    console.log(Session.get('date'));
+    let date = $('#my-datepicker').val();
+    let [day, month, year] = date.split('/')
+    Session.set('date', [day, month, year])
+    console.log(date, day, month, year)
+    if (!Hue.findOne({name:'data', day, month, year})) {
+      Meteor.call("download", day, month, year)
+    }
+  }
+});
 
 Template.sunshow.helpers({
   disabled() {return (Hue.findOne({name:'active'}))? (Hue.findOne({name:'active'}).val)? "disabled" : "" : ""},
-  colormaps: [{cname:"gist_gray"},
-          {cname:"RdPu_r"},
-          {cname:"Set3_r"},
-          {cname:"gray"},
-          {cname:"Set2"},
-          {cname:"gist_ncar_r"},
-          {cname:"gist_ncar"},
-          {cname:"Accent"},
-          {cname:"YlGn"},
-          {cname:"RdYlBu"},
-          {cname:"BrBG_r"},
-          {cname:"Pastel2_r"},
-          {cname:"YlGn_r"},
-          {cname:"hot_r"},
-          {cname:"YlOrRd_r"},
-          {cname:"Spectral_r"},
-          {cname:"BrBG"},
-          {cname:"Purples"},
-          {cname:"terrain_r"},
-          {cname:"PuRd"},
-          {cname:"summer_r"},
-          {cname:"gist_heat"},
-          {cname:"flag_r"},
-          {cname:"gist_earth_r"},
-          {cname:"PuBuGn"},
-          {cname:"PuBu"},
-          {cname:"gist_gray_r"},
-          {cname:"hsv_r"},
-          {cname:"binary_r"},
-          {cname:"PiYG_r"},
-          {cname:"gist_yarg"},
-          {cname:"OrRd"},
-          {cname:"GnBu_r"},
-          {cname:"coolwarm_r"},
-          {cname:"RdYlGn"},
-          {cname:"spectral"},
-          {cname:"Wistia_r"},
-          {cname:"Dark2"},
-          {cname:"PuOr_r"},
-          {cname:"PiYG"},
-          {cname:"seismic"},
-          {cname:"RdPu"},
-          {cname:"Spectral"},
-          {cname:"Reds"},
-          {cname:"Oranges_r"},
-          {cname:"nipy_spectral"},
-          {cname:"Blues"},
-          {cname:"YlOrBr_r"},
-          {cname:"gist_stern_r"},
-          {cname:"CMRmap"},
-          {cname:"PRGn_r"},
-          {cname:"Greens_r"},
-          {cname:"BuGn_r"},
-          {cname:"RdBu"},
-          {cname:"nipy_spectral_r"},
-          {cname:"PuRd_r"},
-          {cname:"Accent_r"},
-          {cname:"jet_r"},
-          {cname:"BuPu"},
-          {cname:"Set1"},
-          {cname:"gist_rainbow"},
-          {cname:"gnuplot2_r"},
-          {cname:"Reds_r"},
-          {cname:"Greys_r"},
-          {cname:"prism"},
-          {cname:"rainbow"},
-          {cname:"Purples_r"},
-          {cname:"bwr_r"},
-          {cname:"Pastel1"},
-          {cname:"YlGnBu_r"},
-          {cname:"gist_yarg_r"},
-          {cname:"spectral_r"},
-          {cname:"hot"},
-          {cname:"RdGy_r"},
-          {cname:"gist_earth"},
-          {cname:"spring"},
-          {cname:"RdBu_r"},
-          {cname:"bone"},
-          {cname:"BuPu_r"},
-          {cname:"cubehelix"},
-          {cname:"autumn"},
-          {cname:"PuOr"},
-          {cname:"brg"},
-          {cname:"brg_r"},
-          {cname:"RdGy"},
-          {cname:"bwr"},
-          {cname:"Dark2_r"},
-          {cname:"Pastel2"},
-          {cname:"cool_r"},
-          {cname:"copper"},
-          {cname:"PuBuGn_r"},
-          {cname:"afmhot_r"},
-          {cname:"binary"},
-          {cname:"OrRd_r"},
-          {cname:"YlOrRd"},
-          {cname:"hsv"},
-          {cname:"afmhot"},
-          {cname:"gist_stern"},
-          {cname:"gnuplot"},
-          {cname:"Blues_r"},
-          {cname:"rainbow_r"},
-          {cname:"Pastel1_r"},
-          {cname:"gnuplot_r"},
-          {cname:"YlGnBu"},
-          {cname:"PRGn"},
-          {cname:"gray_r"},
-          {cname:"bone_r"},
-          {cname:"prism_r"},
-          {cname:"BuGn"},
-          {cname:"PuBu_r"},
-          {cname:"RdYlGn_r"},
-          {cname:"CMRmap_r"},
-          {cname:"YlOrBr"},
-          {cname:"pink_r"},
-          {cname:"Paired_r"},
-          {cname:"Greens"},
-          {cname:"pink"},
-          {cname:"spring_r"},
-          {cname:"seismic_r"},
-          {cname:"Set1_r"},
-          {cname:"Oranges"},
-          {cname:"RdYlBu_r"},
-          {cname:"cubehelix_r"},
-          {cname:"jet"},
-          {cname:"ocean_r"},
-          {cname:"terrain"},
-          {cname:"copper_r"},
-          {cname:"flag"},
-          {cname:"ocean"},
-          {cname:"GnBu"},
-          {cname:"winter"},
-          {cname:"winter_r"},
-          {cname:"autumn_r"},
-          {cname:"gist_heat_r"},
-          {cname:"cool"},
-          {cname:"gnuplot2"},
-          {cname:"Wistia"},
-          {cname:"Set2_r"},
-          {cname:"Set3"},
-          {cname:"Paired"},
-          {cname:"coolwarm"},
-          {cname:"summer"},
-          {cname:"gist_rainbow_r"},
-          {cname:"Greys"}]});
+  colormaps: colormaps
+});
 
 Template.sunshow.events({
   'click .cmap'(event, instance){
     event.preventDefault();
     let cmap=event.target.id;
-    console.log(cmap, event, instance);
-    Meteor.call("hue.activate");
-    //event.preventDefaults();
-    HTTP.get(`http://${IP}/sun/${cmap}`, function(error, result){
-      if(error){
-        console.log("error", error);
-      }
-      if(result){
-         console.log("result", result)
-         if (result.content === "done") {
-           Meteor.call("hue.deactivate");
-         }
-      }
-    });
+    Meteor.call("sunshow", cmap);
   },
   'click .defaultshow'(event, instance){
     event.preventDefault();
     let cmap='hot';
-    console.log(cmap, event, instance);
-    Meteor.call("hue.activate");
-    //event.preventDefaults();
-    HTTP.get(`http://${IP}/sun/${cmap}`, function(error, result){
-      if(error){
-        console.log("error", error);
-      }
-      if(result){
-         console.log("result", result)
-         if (result.content === "done") {
-           Meteor.call("hue.deactivate");
-         }
-      }
-    });
+    Meteor.call("sunshow", cmap);
   },
 })
 
@@ -225,23 +232,10 @@ Template.rungame.helpers({
 
 Template.rungame.events({
   'click button'(event, instance){
-    Meteor.call("hue.activate");
     let speed = $('#speed').val();
     let tours = $('#tours').val();
-    let url = `http://${IP}/run/${speed}/${tours}`;
-    console.log(event, instance, url, speed);
     //event.preventDefaults();
-    HTTP.get(url, function(error, result){
-      if(error){
-        console.log("error", error);
-      }
-      if(result){
-         console.log("result", result)
-         if (result.content === "done") {
-           Meteor.call("hue.deactivate");
-         }
-      }
-    });
+    Meteor.call("rungame", speed, tours)
   },
 })
 
--- kunst/server/main.js
+++ kunst/server/main.js
@@ -1,11 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import { Hue } from '../imports/api/hue.js';
 
+const IP = 'ip322.alb283.cust.comxnet.dk:6785';
+
+
 Meteor.startup(() => {
   // code to run on server at startup
 
   // Set the backend python as inactive
   Meteor.call('hue.deactivate');
+  if (!Hue.findOne({name:'url'})) {
+    Hue.insert({name:'url', url:'https://plot.ly/~piredtu/36'});
+  }
 });
 
 Meteor.methods({
@@ -25,5 +31,55 @@ Meteor.methods({
       Hue.insert({name:'active', val:false});
     }
   },
+  'sunshow'(cmap){
+    Meteor.call("hue.activate");
+    //event.preventDefaults();
+      HTTP.get(`http://${IP}/sun/${cmap}`, function(error, result){
+        if(error){
+          console.log("error", error);
+        }
+        if(result){
+           console.log("result", result)
+           if (result.content === "done") {
+             Meteor.call("hue.deactivate");
+           }
+        }
+      });
+  },
+  'rungame'(speed, tours){
+    Meteor.call("hue.activate");
+    let url = `http://${IP}/run/${speed}/${tours}`;
+    console.log(event, instance, url, speed);
+    HTTP.get(url, function(error, result){
+      if(error){
+        console.log("error", error);
+      }
+      if(result){
+         console.log("result", result)
+         if (result.content === "done") {
+           Meteor.call("hue.deactivate");
+         }
+      }
+    });
+  },
+  'download'(day, month, year){
+    Meteor.call("hue.activate");
+    let url = `http://${IP}/download/${day}/${month}/${year}`;
+    HTTP.get(url, function(error, result){
+      if(error){
+        console.log("error", error);
+      }
+      if(result){
+         console.log("result", result)
+         if (result.content.includes('plot')) {
+           console.log('plot detected:', result.content)
+           Hue.insert({name:'data', day, month, year, url:result.content});
+         } else {
+           console.log('error:', result)
+         }
+         Meteor.call("hue.deactivate");
+      }
+    });
+  }
 
 });
